50ccd6ac43ea2fb6f0cde61c92c3e5f91d6cba9f
Tatiana <Tatiana_novik@mail.ru>
2022-05-12 00:31:58 +0700

    Task_3_else

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+string s_a = Console.ReadLine ();
+string s_b = Console.ReadLine ();
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a > b) {
+    Console.WriteLine ($"{a} больше {b}");
+}
+else {
+    Console.WriteLine ( $"{b} больше {a}");
+}
