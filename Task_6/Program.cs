387b09f155e318924e7770765426b869476fc740
Tatiana <Tatiana_novik@mail.ru>
2022-05-12 13:45:00 +0700

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, 
+// которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+int i = 1;
+while (i < a){ 
+    if (0 == i % 2){
+    Console.WriteLine($"{i}");
+}
+    i += 1; 
+}
